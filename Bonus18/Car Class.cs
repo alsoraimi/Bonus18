4912defb9b829038ce2a6c6df0e4123417c1f566
alsoraimi <alsoraimi46@gmail.com>
2017-04-21 14:29:30 -0400

    initial commit, add classes and properties, nothing at main yet

---
+++ Bonus18/Car Class.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class CarClass
+    {
+        #region
+        private string Make;
+        private string Model;
+        private int Year;
+        private double Price;
+        #endregion
+
+        public string MakeofCar
+        {
+            set
+            {
+                Make = value;
+            }
+            get
+            {
+                return Make;
+            }
+        }
+        public string ModelofCar
+        {
+            set
+            {
+                Model = value;
+            }
+            get
+            {
+                return Model;
+            }
+        }
+        public int YearofCar
+        {
+            set
+            {
+                Year = value;
+            }
+            get
+            {
+                return Year;
+            }
+        }
+        public double PriceofCar
+        {
+            set
+            {
+                Price = value;
+            }
+            get
+            {
+                return Price;
+            }
+        }
+        public CarClass (string MakeofCarInput, 
+            string ModelofCarInput,
+            int YearofCarInput,
+            double PriceofCarInput)
+        {
+
+            Make = MakeofCarInput;
+            Model = ModelofCarInput;
+            Year = YearofCarInput;
+            Price = PriceofCarInput;
+        }
+        public CarClass()
+        {
+            Make = Console.ReadLine();
+            Model = Console.ReadLine();
+            Year = Console.ReadLine();
+            Price = Console.ReadLine();
+
+        }
+
+
+    }
+}
