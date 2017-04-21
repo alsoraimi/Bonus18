d5d6dc61e6249ca3f4cc119b5dba93701686b70e
alsoraimi <alsoraimi46@gmail.com>
2017-04-21 15:57:55 -0400

    fixed formatting and add validation for price and year

--- Bonus18/Program.cs
+++ Bonus18/Program.cs
@@ -12,25 +12,65 @@ namespace Bonus18
         {
             CarClass Car1 = new CarClass();
             Console.WriteLine("Enter Make of Car");
-            Car1.ModelofCar = Console.ReadLine();
-            Console.WriteLine("Enter Model of Car");
             Car1.MakeofCar = Console.ReadLine();
-            Console.WriteLine("Enter Year of Car");
-            Console.WriteLine("Please enter price of car"); 
-            Car1.PriceofCar = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Model of Car");
+            Car1.ModelofCar = Console.ReadLine();
+            Car1.YearofCar = int.Parse(GetValidYear());
+            Car1.PriceofCar = double.Parse(GetValidPrice());
+            Console.Clear();
+
+            Console.WriteLine("Current Inventory: ");
+            Console.WriteLine();
 
 
-            Console.WriteLine("Make of car is: " + Car1.MakeofCar);
+            Console.WriteLine(Car1.MakeofCar + "        " + Car1.ModelofCar + "         " + Car1.YearofCar + "         " + Car1.PriceofCar);
             Console.WriteLine();
-            Console.WriteLine("Model of car is: " + Car1.ModelofCar);
-            Console.WriteLine();
-            Console.WriteLine("Year of Car is: " + Car1.YearofCar);
-            Console.WriteLine();
-            Console.WriteLine("price of car is: " +Car1.PriceofCar);
-            
+            //Console.WriteLine( Car1.ModelofCar);
+            //Console.WriteLine();
+            //Console.WriteLine( Car1.YearofCar);
+            //Console.WriteLine();
+            //Console.WriteLine(Car1.PriceofCar);
+
 
 
 
         }
+
+        public static string GetValidYear()
+        {
+
+
+            int value;
+            Console.WriteLine("Enter Year of Car");
+            string YearofCar = Console.ReadLine();
+            while (!int.TryParse(YearofCar, out value))
+            {
+                Console.WriteLine("ERROR, Please enter a valid number");
+                YearofCar = Console.ReadLine();
+
+            }
+            return YearofCar;
+
+        }
+
+        public static string GetValidPrice()
+        {
+
+
+            double value;
+            Console.WriteLine("Enter Price of Car");
+            string PriceOfCar = Console.ReadLine();
+            while (!double.TryParse(PriceOfCar, out value))
+            {
+                Console.WriteLine("ERROR, Please enter a valid number");
+                PriceOfCar = Console.ReadLine();
+
+            }
+            return PriceOfCar;
+        }
+
     }
-}
+    }
+
+
+
