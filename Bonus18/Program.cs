89f87a4569bfc7b1b625d3c4692b2410d44475c7
alsoraimi <alsoraimi46@gmail.com>
2017-04-22 17:14:53 -0400

    added loop for user to continue

--- Bonus18/Program.cs
+++ Bonus18/Program.cs
@@ -10,54 +10,61 @@ namespace Bonus18
     {
         static void Main(string[] args)
         {
-
-            Console.Title ="Welcome to the Grand Circus Motors admin console!!!";  // makes title for entire console
-
-            Console.WriteLine("How many cars are you entering? ");
-            int numCars = int.Parse(Console.ReadLine());
-
-
-            List<CarClass> CarList = new List<CarClass>(numCars);   //added so user can input more than one vehicle
-
-            for (int i = 0; i < numCars; i++)
+            bool loop = true;
+            do
             {
-                Console.WriteLine("\nWould you like to enter data for Car {0}? (y = yes n = continue)", i + 1);
-                string defaultCar = Console.ReadLine().ToLower();
+                Console.Title = "Welcome to the Grand Circus Motors admin console!!!";  // makes title for entire console
 
-                if (defaultCar == "y")
-                {
-                    
-                    CarClass Car = new CarClass();     // creates new class per new user input for car
-                    Console.WriteLine("Please enter Make of Car");
-                    Car.MakeofCar = Console.ReadLine();
-                    Console.WriteLine("Please Enter Model of Car");
-                    Car.ModelofCar = Console.ReadLine();
-                    Car.YearofCar = int.Parse(GetValidYear());
-                    Car.PriceofCar = double.Parse(GetValidPrice());
-                    CarList.Add(Car);
-                    
-                }
-                else if (defaultCar == "n")
-                {
-                    CarClass Car = new CarClass();   
-                    CarList.Add(Car);
-                }
-                else
-                {
-                    Console.WriteLine("ERROR INVALID INPUT");
-                    break;
-                }
+                Console.WriteLine("How many cars are you entering? ");
+                int numCars = int.Parse(Console.ReadLine());
 
-            }
-            Console.Clear();
-            Console.WriteLine("Car Data:");
-            Console.WriteLine("Year\tMake\tModel\tPrice");
-            foreach (CarClass item in CarList)
+
+                List<CarClass> CarList = new List<CarClass>(numCars);   //added so user can input more than one vehicle
+
+                for (int i = 0; i < numCars; i++)
+                {
+                    Console.WriteLine("\nWould you like to enter data for Car {0}? (y = yes n = continue)", i + 1);
+                    string defaultCar = Console.ReadLine().ToLower();
+
+                    if (defaultCar == "y")
+                    {
+
+                        CarClass Car = new CarClass();     // creates new class per new user input for car
+                        Console.WriteLine("Please enter Make of Car");
+                        Car.MakeofCar = Console.ReadLine();
+                        Console.WriteLine("Please Enter Model of Car");
+                        Car.ModelofCar = Console.ReadLine();
+                        Car.YearofCar = int.Parse(GetValidYear());
+                        Car.PriceofCar = double.Parse(GetValidPrice());
+                        CarList.Add(Car);
+
+                    }
+                    else if (defaultCar == "n")
+                    {
+                        CarClass Car = new CarClass();
+                        CarList.Add(Car);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR INVALID INPUT");
+                        break;
+                    }
+
+                }
+                Console.Clear();
+                Console.WriteLine("Car Data:");
+                Console.WriteLine("Year\tMake\tModel\tPrice");
+                foreach (CarClass item in CarList)
                 // foreach to loop through items in CarList and outputs them
-            {
-                Console.WriteLine("{0}\t{1}\t{2}\t${3}.", item.YearofCar, item.MakeofCar, item.ModelofCar, item.PriceofCar);
-            }
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t${3}.", item.YearofCar, item.MakeofCar, item.ModelofCar, item.PriceofCar);
+                }
+                if (!ContinueLoop())
+                {
+                    loop = false;
+                }
 
+            } while (loop);
         }
 
         public static string GetValidYear()
@@ -93,8 +100,33 @@ namespace Bonus18
             return PriceOfCar;
         }  //validation for price
 
+        public static bool ContinueLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to go again, ? (Y/N)"); // add user input if they want to try again
+                string input = Console.ReadLine().ToUpper();
+                if (input == "Y")
+                {
+                    Console.WriteLine();
+                    return true;
+                }
+
+                else if (input == "N")
+                {
+                    Console.WriteLine("thanks for trying!");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR, ENTER Y OR N");
+
+                }
+            }
+
+        }
     }
-    }
+}
 
 
 
